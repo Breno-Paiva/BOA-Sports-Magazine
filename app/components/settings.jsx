f991d146d1c2402c136b8dc4cffa56b72ae7bfa5
Breno Paiva <breno.paiva93@gmail.com>
2017-08-18 11:22:17 -0400

    Render name from store

--- app/components/settings.jsx
+++ app/components/settings.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 class Settings extends React.Component{
   constructor(props){
     super(props);
+    this.name = props.store.getState().name
   }
 
   render(){
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>SportsMag Settings</h1>
+        <h2>Name</h2>
+        <h4>{this.name.first}</h4>
+        <h4>{this.name.last}</h4>
       </div>
     );
   }
