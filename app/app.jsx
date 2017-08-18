c0e54d2b21aba1c5733fd82257ae18802cf30723
Breno Paiva <breno.paiva93@gmail.com>
2017-08-18 11:41:57 -0400

    Render address and teams from state

--- app/app.jsx
+++ app/app.jsx
@@ -4,7 +4,10 @@ import configureStore from './store';
 import Settings from './components/settings';
 
 document.addEventListener("DOMContentLoaded", ()=>{
-  const preloadedState = { name: { first: 'Harry', last: 'Potter' } };
+  const preloadedState = { name: { first: 'Harry', last: 'Potter' },
+                        address: { street: "123 Bowl Lane", city: "New York", state: "NY", zip: "10021" },
+                           team: ["NY Knicks", "NY Giants"]
+};
   const store = configureStore(preloadedState);
   const root = document.getElementById("root");
   ReactDOM.render(<Settings store={store} />, root);
--- app/components/settings.jsx
+++ app/components/settings.jsx
@@ -3,16 +3,36 @@ import React from 'react';
 class Settings extends React.Component{
   constructor(props){
     super(props);
-    this.name = props.store.getState().name
+    this.name = props.store.getState().name;
+    this.address = props.store.getState().address;
+    this.teams = props.store.getState().team;
   }
 
   render(){
     return (
       <div>
         <h1>SportsMag Settings</h1>
-        <h2>Name</h2>
-        <h4>{this.name.first}</h4>
-        <h4>{this.name.last}</h4>
+        <div>
+          <h2>Name</h2>
+          <h4>{this.name.first} {this.name.last}</h4>
+        </div>
+        <div>
+          <h2>Address</h2>
+          <h4>{this.address.street}</h4>
+          <h4>{this.address.city}, {this.address.state} {this.address.zip}</h4>
+        </div>
+        <div>
+          <h2>Teams</h2>
+          <ol>
+            {
+              this.teams.map(team => (
+                <li>
+                  <h4>{team}</h4>
+                </li>
+              ))
+            }
+         </ol>
+        </div>
       </div>
     );
   }
