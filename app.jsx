06ad5558c729b120aba7623025745a683a981197
Breno Paiva <breno.paiva93@gmail.com>
2017-08-18 10:04:53 -0400

    Set up initial configuration

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Settings from './settings';
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(<Settings />, document.getElementById('main'));
+});
